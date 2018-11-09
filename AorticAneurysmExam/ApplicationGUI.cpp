977ee9148bb3810f378bca128b70a9178a67b1ba
Andreas Østvik <a.ostvik@gmail.com>
2018-11-09 14:39:53 +0100

    Updated with robot connection and visualization utilities.

--- AorticAneurysmExam/ApplicationGUI.cpp
+++ AorticAneurysmExam/ApplicationGUI.cpp
@@ -11,35 +11,82 @@
 #include <QListWidget>
 #include <QDirIterator>
 #include <QMessageBox>
+#include <QGroupBox>
+#include <QProgressDialog>
+#include <QDir>
+#include <QTabWidget>
+
 #include "FAST/Visualization/MultiViewWindow.hpp"
 #include <FAST/Visualization/SegmentationRenderer/SegmentationRenderer.hpp>
 #include <FAST/Visualization/VertexRenderer/VertexRenderer.hpp>
 #include <FAST/Streamers/MeshFileStreamer.hpp>
 #include <FAST/Exporters/VTKMeshFileExporter.hpp>
 #include <FAST/Importers/VTKMeshFileImporter.hpp>
-#include <QProgressDialog>
-
-
+#include "FAST/Visualization/TriangleRenderer/TriangleRenderer.hpp"
 
 namespace fast {
 
-ApplicationGUI::ApplicationGUI() {
+ApplicationGUI::ApplicationGUI() :
+        mGraphicsFolderName("AorticAneurysmExam/widgets/icons/")
+{
+    //QDir dir = QDir::current(); dir.cdUp(); dir.cdUp();
+    //mGraphicsFolderName = dir.path()+mGraphicsFolderName;
+
     mRobotInterface = RobotInterfacePtr(new RobotInterface);
+
     setupUI();
+    setupConnections();
 }
 
-void ApplicationGUI::toggleRobotConnection() {
-    mRobotInterface->robot.configure(corah::Manipulator::UR5,"10.218.140.114",30003);
+void ApplicationGUI::setupConnections()
+{
+    connect(robotConnectButton,&QPushButton::clicked,this,&ApplicationGUI::robotConnectButtonSlot);
+    connect(robotDisconnectButton,&QPushButton::clicked,this,&ApplicationGUI::robotDisconnectButtonSlot);
+    connect(robotShutdownButton,&QPushButton::clicked,this,&ApplicationGUI::robotShutdownButtonSlot);
+
+    QObject::connect(cameraConnectButton, &QPushButton::clicked, std::bind(&ApplicationGUI::connectToCamera, this));
+    QObject::connect(cameraDisconnectButton, &QPushButton::clicked, std::bind(&ApplicationGUI::disconnectFromCamera, this));
+}
+
+void ApplicationGUI::robotConnectButtonSlot()
+{
+    mRobotInterface->robot.configure(corah::Manipulator::UR5,robotIPLineEdit->text(),30003);
     mRobotInterface->robot.start();
 
-    if(mRobotInterface->robot.isConnected())
+    if(mRobotInterface->robot.isConnected() && !robotConnectButton->isChecked())
     {
-        std::cout << mRobotInterface->robot.getCurrentState().jointConfiguration << std::endl;
+        robotConnectButton->toggle();
     }
+    else if(!mRobotInterface->robot.isConnected() && robotConnectButton->isChecked())
+    {
+        robotConnectButton->toggle();
+    }
+
+    View* view3D = getView(1);
+    stopComputationThread();
+    setupRobotManipulatorVisualization();
+    view3D->reinitialize();
+    startComputationThread();
+}
+
+void ApplicationGUI::robotDisconnectButtonSlot()
+{
+    mRobotInterface->robot.disconnectFromRobot();
+
+    if(!mRobotInterface->robot.isConnected() && robotConnectButton->isChecked())
+        robotConnectButton->toggle();
+
+}
+
+void ApplicationGUI::robotShutdownButtonSlot()
+{
+    mRobotInterface->robot.shutdown();
 }
 
 
-void ApplicationGUI::toggleCameraConnection() {
+void ApplicationGUI::connectToCamera() {
+    cameraConnectButton->setChecked(0);
+
     View* view2D = getView(0);
     View* view3D = getView(1);
 
@@ -77,9 +124,38 @@ void ApplicationGUI::toggleCameraConnection() {
     view2D->addRenderer(renderer);
     view2D->reinitialize();
 
+    setupRobotToolVisualization();
+    setupRobotManipulatorVisualization();
+
     startComputationThread();
 }
 
+void ApplicationGUI::disconnectFromCamera() {
+    mStreamer->stop();
+    mStreamer->stopPipeline();
+    mTracking->stopRecording();
+    mTracking->stopPipeline();
+}
+
+void ApplicationGUI::setupRobotToolVisualization()
+{
+    RobotTool *tool = new RobotTool("AorticAneurysmExam/visualization/CADModels/5S-Probe.vtk");
+    View* view3D = getView(1);
+    view3D->addRenderer(tool->getRenderer());
+}
+
+void ApplicationGUI::setupRobotManipulatorVisualization() {
+    View* view3D = getView(1);
+
+    RobotManipulator *manipulator = new RobotManipulator(mRobotInterface);
+    view3D->addRenderer(manipulator->getRenderer(0));
+    view3D->addRenderer(manipulator->getRenderer(1));
+    view3D->addRenderer(manipulator->getRenderer(2));
+    view3D->addRenderer(manipulator->getRenderer(3));
+    view3D->addRenderer(manipulator->getRenderer(4));
+    view3D->addRenderer(manipulator->getRenderer(5));
+    view3D->addRenderer(manipulator->getRenderer(6));
+}
 
 void ApplicationGUI::setupUI()
 {
@@ -89,14 +165,17 @@ void ApplicationGUI::setupUI()
     const int menuWidth = 300;
 
     setTitle("Aortic Aneurysm Exam");
-    setWidth(1024 + menuWidth);
-    setHeight(848);
+    setWidth(1920);
+    setHeight(1080);
     enableMaximized();
+
     view3D->set3DMode();
     view3D->setBackgroundColor(Color::Black());
+    view3D->setFixedWidth(750);
 
     view2D->set2DMode();
     view2D->setBackgroundColor(Color::Black());
+    view2D->setFixedWidth(550);
 
     QVBoxLayout* menuLayout = new QVBoxLayout;
     menuLayout->setAlignment(Qt::AlignTop);
@@ -106,37 +185,45 @@ void ApplicationGUI::setupUI()
     title->setText("<div style=\"text-align: center; font-weight: bold; font-size: 24px;\">Aortic Aneurysm Exam</div>");
     menuLayout->addWidget(title);
 
-    // Quit butto1.0f;//n
+    //QPushButton* cameraConnectionButton = new QPushButton;
+    //cameraConnectionButton->setText("Connect to camera");
+    //cameraConnectionButton->setStyleSheet("QPushButton { background-color: green; color: white; }");
+    //cameraConnectionButton->setFixedWidth(menuWidth);
+    //menuLayout->addWidget(cameraConnectionButton);
+
+//    QPushButton* robotConnectionButton = new QPushButton;
+//    robotConnectionButton->setText("Connect to robot");
+//    robotConnectionButton->setStyleSheet("QPushButton {background-color:green; color:white; font-size=36px}");
+//    robotConnectionButton->setFixedWidth(menuWidth);
+//    QObject::connect(robotConnectionButton, &QPushButton::clicked, std::bind(&ApplicationGUI::toggleRobotConnection, this));
+//    menuLayout->addWidget(robotConnectionButton);
+//
+//    QPushButton* robotVisualizeButton = new QPushButton;
+//    robotVisualizeButton->setText("Visualize links");
+//    robotVisualizeButton->setStyleSheet("QPushButton { background-color: blue; color: white; }");
+//    robotVisualizeButton->setFixedWidth(menuWidth);
+//    //QObject::connect(robotVisualizeButton, &QPushButton::clicked, std::bind(&RobotInterface::startVisualization, this));
+//    menuLayout->addWidget(robotVisualizeButton);
+
+    setRobotConnectionLayout(menuLayout);
+    setCameraConnectionLayout(menuLayout);
+
+    mMoveLayout = new RobotManualMoveLayout(mRobotInterface);
+
+    QWidget *testWidget = new QWidget;
+
+    tabWidget = new QTabWidget;
+    tabWidget->addTab(testWidget, "Workflow");
+    tabWidget->addTab(mMoveLayout->tabWindow, "Robot manual motion");
+    menuLayout->addWidget(tabWidget);
+
+    // Quit button
     QPushButton* quitButton = new QPushButton;
     quitButton->setText("Quit (q)");
     quitButton->setStyleSheet("QPushButton { background-color: red; color: white; }");
     quitButton->setFixedWidth(menuWidth);
     QObject::connect(quitButton, &QPushButton::clicked, std::bind(&Window::stop, this));
-    menuLayout->addWidget(quitButton);
-
-    QPushButton* cameraConnectionButton = new QPushButton;
-    cameraConnectionButton->setText("Connect to camera");
-    cameraConnectionButton->setStyleSheet("QPushButton { background-color: green; color: white; }");
-    cameraConnectionButton->setFixedWidth(menuWidth);
-    QObject::connect(cameraConnectionButton, &QPushButton::clicked, std::bind(&ApplicationGUI::toggleCameraConnection, this));
-    menuLayout->addWidget(cameraConnectionButton);
-
-    QPushButton* robotConnectionButton = new QPushButton;
-    robotConnectionButton->setText("Connect to robot");
-    robotConnectionButton->setStyleSheet("QPushButton { background-color: green; color: white; }");
-    robotConnectionButton->setFixedWidth(menuWidth);
-    QObject::connect(robotConnectionButton, &QPushButton::clicked, std::bind(&ApplicationGUI::toggleRobotConnection, this));
-    menuLayout->addWidget(robotConnectionButton);
-
-    QPushButton* robotVisualizeButton = new QPushButton;
-    robotVisualizeButton->setText("Connect to robot");
-    robotVisualizeButton->setStyleSheet("QPushButton { background-color: blue; color: white; }");
-    robotVisualizeButton->setFixedWidth(menuWidth);
-    //QObject::connect(robotVisualizeButton, &QPushButton::clicked, std::bind(&RobotInterface::startVisualization, this));
-    menuLayout->addWidget(robotVisualizeButton);
-
-    mMoveLayout = new RobotManualMoveLayout(mRobotInterface);
-    menuLayout->addLayout(mMoveLayout->getLayout());
+    menuLayout->addWidget(quitButton,0,Qt::AlignBottom);
 
     QHBoxLayout* layout = new QHBoxLayout;
     layout->addLayout(menuLayout);
@@ -144,5 +231,66 @@ void ApplicationGUI::setupUI()
     layout->addWidget(view2D);
     mWidget->setLayout(layout);
 }
+
+void ApplicationGUI::setCameraConnectionLayout(QVBoxLayout *parent)
+{
+    QGroupBox* group = new QGroupBox("Camera");
+    group->setFlat(true);
+    parent->addWidget(group,0,Qt::AlignTop);
+
+    QGridLayout *mainLayout = new QGridLayout();
+    group->setLayout(mainLayout);
+
+    cameraConnectButton = new QPushButton();
+    mainLayout->addWidget(cameraConnectButton,0,0,1,1);
+
+    QIcon icon;
+    icon.addFile(mGraphicsFolderName+"network-idle.ico", QSize(), QIcon::Normal, QIcon::Off);
+    icon.addFile(mGraphicsFolderName+"network-transmit-receive.ico", QSize(), QIcon::Normal, QIcon::On);
+    cameraConnectButton->setIcon(icon);
+    cameraConnectButton->setToolTip("Connect to robot");
+    cameraConnectButton->setText("Connect");
+    cameraConnectButton->setCheckable(true);
+    cameraConnectButton->setStyleSheet("QPushButton:checked { background-color: none; }");
+
+    cameraDisconnectButton = new QPushButton(QIcon(mGraphicsFolderName+"network-offline.ico"),"Disconnect");
+    mainLayout->addWidget(cameraDisconnectButton,0,1,1,1);
+}
+
+void ApplicationGUI::setRobotConnectionLayout(QVBoxLayout *parent)
+{
+    QGroupBox* group = new QGroupBox("Robot");
+    group->setFlat(true);
+    parent->addWidget(group,0,Qt::AlignTop);
+
+    QGridLayout *mainLayout = new QGridLayout();
+    group->setLayout(mainLayout);
+
+    int row = 0;
+    robotIPLineEdit = new QLineEdit();
+    robotConnectButton = new QPushButton();
+    mainLayout->addWidget(new QLabel("IP Address: "), row, 0, 1, 1);
+    mainLayout->addWidget(robotIPLineEdit, row, 1,1,1);
+    mainLayout->addWidget(robotConnectButton,row,2,1,1);
+
+    robotIPLineEdit->setText("localhost"); // 10.218.140.114
+    robotIPLineEdit->setAlignment(Qt::AlignCenter);
+
+    QIcon icon;
+    icon.addFile(mGraphicsFolderName+"network-idle.ico", QSize(), QIcon::Normal, QIcon::Off);
+    icon.addFile(mGraphicsFolderName+"network-transmit-receive.ico", QSize(), QIcon::Normal, QIcon::On);
+    robotConnectButton->setIcon(icon);
+    robotConnectButton->setToolTip("Connect to robot");
+    robotConnectButton->setText("Connect");
+    robotConnectButton->setCheckable(true);
+    robotConnectButton->setStyleSheet("QPushButton:checked { background-color: none; }");
+
+    row++;
+    robotShutdownButton = new QPushButton(QIcon(mGraphicsFolderName+"application-exit-4.png"),"Shutdown");
+    robotDisconnectButton = new QPushButton(QIcon(mGraphicsFolderName+"network-offline.ico"),"Disconnect");
+    mainLayout->addWidget(robotShutdownButton,row,0,1,1);
+    mainLayout->addWidget(robotDisconnectButton,row,2,1,1);
+}
+
 
 }
--- AorticAneurysmExam/ApplicationGUI.hpp
+++ AorticAneurysmExam/ApplicationGUI.hpp
@@ -2,12 +2,13 @@
 #define FAST_KINECT_TRACKING_GUI_HPP_
 
 #include "FAST/Visualization/MultiViewWindow.hpp"
-
+#include "visualization/robotVisualization.h"
 #include "widgets/robotManualMoveTab.h"
 #include "RobotInterface.h"
 
 class QPushButton;
 class QLabel;
+class QTabWidget;
 
 namespace fast {
 
@@ -17,19 +18,39 @@ class KinectTracking;
 class ApplicationGUI : public MultiViewWindow {
     FAST_OBJECT(ApplicationGUI)
 
-    private:
-        ApplicationGUI();
-        RobotInterfacePtr mRobotInterface;
+private slots:
+    void robotConnectButtonSlot();
+    void robotDisconnectButtonSlot();
+    void robotShutdownButtonSlot();
 
-        SharedPointer<KinectStreamer> mStreamer;
-        SharedPointer<KinectTracking> mTracking;
+private:
+    ApplicationGUI();
+    RobotInterfacePtr mRobotInterface;
 
-        RobotManualMoveLayout* mMoveLayout;
+    SharedPointer<KinectStreamer> mStreamer;
+    SharedPointer<KinectTracking> mTracking;
 
-        void toggleCameraConnection();
-        void toggleRobotConnection();
+    RobotManualMoveLayout* mMoveLayout;
 
-        void setupUI();
+    void connectToCamera();
+    void disconnectFromCamera();
+
+    void setupUI();
+
+    QString mGraphicsFolderName;
+    QLineEdit *robotIPLineEdit;
+    QPushButton *robotConnectButton, *robotDisconnectButton,*robotShutdownButton;
+    QPushButton *cameraConnectButton, *cameraDisconnectButton;
+    QTabWidget *tabWidget;
+
+    void setRobotConnectionLayout(QVBoxLayout *parent);
+    void setCameraConnectionLayout(QVBoxLayout *parent);
+
+    void setupRobotToolVisualization();
+    void setupRobotManipulatorVisualization();
+
+    void setupConnections();
+
 };
 
 }
