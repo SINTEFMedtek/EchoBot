18862a81fbd25f009d5268431c0c232ad0006709
Andreas Østvik <a.ostvik@gmail.com>
2019-03-13 12:08:11 +0100

    Added optional for enabling clarius streaming. Not optimal, should be shipped with FAST.

--- cmake/Requirements.cmake
+++ cmake/Requirements.cmake
@@ -54,6 +54,26 @@ else(ECHOBOT_BUILD_FAST)
     list(APPEND ECHOBOT_SYSTEM_LIBRARIES ${FAST_LIBRARY_DIRS})
 endif(ECHOBOT_BUILD_FAST)
 
+# Clarius streaming
+option(ECHOBOT_ENABLE_CLARIUS_STREAMING "Enable streaming from Clarius probe." ON)
+if(ECHOBOT_ENABLE_CLARIUS_STREAMING)
+    # User has to supply the path to the claris sdk
+    set(CLARIUS_SDK_DIR "NOT_SET" CACHE PATH "Path to the clarius listen API.")
+    if(${CLARIUS_SDK_DIR} STREQUAL "NOT_SET")
+        message(FATAL_ERROR "-- Clarius ultrasound module was enabled, but Clarius SDK dir has not been set in CMake")
+    else()
+        message(STATUS "-- Clarius ultrasound module enabled. Clarius SDK dir set to: ${CLARIUS_SDK_DIR}")
+    endif()
+
+    list(APPEND ECHOBOT_INCLUDE_DIRS ${CLARIUS_SDK_DIR}/include/)
+    if(WIN32)
+        list(APPEND LIBRARIES ${CLARIUS_SDK_DIR}/lib/listen.lib)
+    else()
+        list(APPEND LIBRARIES ${CLARIUS_SDK_DIR}/lib/liblisten.so)
+    endif()
+endif()
+
+
 # Corah
 if(ECHOBOT_BUILD_CORAH)
     include(cmake/ExternalCorah.cmake)
