228dc43662679341065172a9cb535b29b1441579
Andreas Østvik <a.ostvik@gmail.com>
2019-05-30 12:03:41 +0200

    Updated namespace.

--- applications/AorticAneurysmExam/main.cpp
+++ applications/AorticAneurysmExam/main.cpp
@@ -1,5 +1,7 @@
 #include "ApplicationGUI.hpp"
 
+using namespace echobot;
+
 int main() {
     ApplicationGUI::pointer window = ApplicationGUI::New();
     window->start(STREAMING_MODE_NEWEST_FRAME_ONLY);
