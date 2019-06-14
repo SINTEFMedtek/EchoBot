17102e16ffef8c844c7d3a7d4e527efe3e79b822
Andreas Østvik <a.ostvik@gmail.com>
2019-06-14 15:34:35 +0200

    Changed naming.

---
+++ cmake/ExternalRomocc.cmake
@@ -0,0 +1,31 @@
+# Download and set up Eigen
+
+include(cmake/Externals.cmake)
+
+ExternalProject_Add(romocc
+        PREFIX ${ECHOBOT_EXTERNAL_BUILD_DIR}/romocc
+        BINARY_DIR ${ECHOBOT_EXTERNAL_BUILD_DIR}/romocc
+        GIT_REPOSITORY "ssh://git@github.com/androst/libromocc.git"
+        GIT_TAG "master"
+        INSTALL_DIR ${ECHOBOT_EXTERNAL_INSTALL_DIR}
+        CMAKE_CACHE_ARGS
+            -DCMAKE_PREFIX_PATH:PATH=${ECHOBOT_EXTERNAL_INSTALL_DIR}/lib/cmake/Qt5
+            -DCMAKE_PLUGIN_DIR:PATH=${ECHOBOT_EXTERNAL_INSTALL_DIR}/plugins
+            -DCMAKE_BUILD_TYPE:STRING=Release
+            -DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF
+            -DCMAKE_INSTALL_MESSAGE:BOOL=LAZY
+            -DCMAKE_INSTALL_PREFIX:PATH=${ECHOBOT_EXTERNAL_INSTALL_DIR}
+            -DROMOCC_BUILD_TESTS:BOOL=OFF
+            -DROMOCC_BUILD_EXAMPLES:BOOL=OFF
+            -DROMOCC_BUILD_URSIMULATOR:BOOL=OFF
+        )
+
+if(WIN32)
+    set(ROMOCC_LIBRARY romocc.lib)
+else()
+    set(ROMOCC_LIBRARY ${CMAKE_SHARED_LIBRARY_PREFIX}romocc${CMAKE_SHARED_LIBRARY_SUFFIX})
+endif()
+
+list(APPEND ECHOBOT_INCLUDE_DIRS ${install_dir}/fast/include)
+list(APPEND LIBRARIES ${ROMOCC_LIBRARY})
+list(APPEND ECHOBOT_EXTERNAL_DEPENDENCIES romocc)
