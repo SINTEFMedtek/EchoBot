f5fe8373dc9fbb871039b0cef1759f7b8a809abc
androst <a.ostvik@gmail.com>
2019-08-15 09:30:18 +0200

    Resolved some issues with linking the Clarius library. Need to find a better solution.

--- cmake/Requirements.cmake
+++ cmake/Requirements.cmake
@@ -61,21 +61,21 @@ if(ECHOBOT_ENABLE_CLARIUS_STREAMING)
     # User has to supply the path to the claris sdk
     set(CLARIUS_SDK_DIR "NOT_SET" CACHE PATH "Path to the clarius listen API.")
     if(${CLARIUS_SDK_DIR} STREQUAL "NOT_SET")
-        message(FATAL_ERROR "-- Clarius ultrasound module was enabled, but Clarius SDK dir has not been set in CMake")
+        message(FATAL_ERROR "Clarius ultrasound module was enabled, but Clarius SDK dir has not been set in CMake")
     else()
-        message(STATUS "-- Clarius ultrasound module enabled. Clarius SDK dir set to: ${CLARIUS_SDK_DIR}")
+        message(STATUS "Clarius ultrasound module enabled. Clarius SDK dir set to: ${CLARIUS_SDK_DIR}")
     endif()
 
-    list(APPEND ECHOBOT_INCLUDE_DIRS ${CLARIUS_SDK_DIR}/include/)
+    list(APPEND ECHOBOT_INCLUDE_DIRS ${CLARIUS_SDK_DIR}/include)
     if(WIN32)
         list(APPEND LIBRARIES ${CLARIUS_SDK_DIR}/lib/listen.lib)
     else()
-        list(APPEND LIBRARIES ${CLARIUS_SDK_DIR}/lib/liblisten.so)
+        list(APPEND LIBRARIES liblisten.so)
+        link_directories(${CLARIUS_SDK_DIR}/lib/)
     endif()
 endif()
 
-
-# Corah
+# Romocc
 if(ECHOBOT_BUILD_ROMOCC)
     include(cmake/ExternalRomocc.cmake)
 else(ECHOBOT_BUILD_ROMOCC)
