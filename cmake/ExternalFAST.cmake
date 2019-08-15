cc5cc0f835bf0d1dc5a94e6c3ce1d7d6a6321d2a
androst <a.ostvik@gmail.com>
2019-08-15 09:22:36 +0200

    Added CMAKE arguments for building FAST in project.

--- cmake/ExternalFAST.cmake
+++ cmake/ExternalFAST.cmake
@@ -5,32 +5,37 @@ include(cmake/Externals.cmake)
 ExternalProject_Add(FAST
         PREFIX ${ECHOBOT_EXTERNAL_BUILD_DIR}/FAST
         BINARY_DIR ${ECHOBOT_EXTERNAL_BUILD_DIR}/FAST
-        GIT_REPOSITORY "https://github.com/smistad/FAST.git"
-        GIT_TAG "ca95829b949d72d5a5cc34bd4e58deefc10097b6"
+        GIT_REPOSITORY "https://github.com/androst/FAST.git"
+        GIT_TAG "echobot"
+        CMAKE_ARGS
+            -DFAST_MODULE_OpenIGTLink=ON
+            -DFAST_MODULE_RealSense=ON
+            -DFAST_MODULE_Visualization=ON
+            -DFAST_MODULE_TensorFlow=ON
+            -DFAST_BUILD_TensorFlow_CUDA=ON
+            -DFAST_MODULE_WholeSlideImaging=ON
+            -DFAST_MODULE_Clarius=ON
+            -DFAST_BUILD_TOOLS=OFF
+            -DCLARIUS_SDK_DIR=${CLARIUS_SDK_DIR}
         CMAKE_CACHE_ARGS
             -DOpenCL_INCLUDE_DIR:PATH=${OpenCL_INCLUDE_DIR}
             -DOpenCL_LIBRARY:PATH=${OpenCL_LIBRARY}
+            -DCMAKE_INSTALL_PREFIX:PATH=${ECHOBOT_EXTERNAL_BUILD_DIR}
             -DCMAKE_BUILD_TYPE:STRING=Release
             -DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF
             -DCMAKE_INSTALL_MESSAGE:BOOL=LAZY
-            -DBUILD_TESTING:BOOL=OFF
-            -DFAST_BUILD_TESTS:BOOL=ON
-            -DFAST_BUILD_EXAMPLES:BOOL=ON
-            -DFAST_BUILD_TOOLS:BOOL=ON
-            -DCMAKE_INSTALL_PREFIX:PATH=${ECHOBOT_EXTERNAL_BUILD_DIR}/FAST
-            -DFAST_MODULE_Visualization:BOOL=ON
-            -DFAST_MODULE_OpenIGTLink:BOOL=ON
-            -DFAST_MODULE_RealSense:BOOL=ON
-            -DFAST_MODULE_TensorFlow:BOOL=OFF
-            -DFAST_BUILD_QT5:BOOL=OFF
 )
-ExternalProject_Get_Property(FAST install_dir)
 
 if(WIN32)
     set(FAST_LIBRARY FAST.lib)
 else()
     set(FAST_LIBRARY ${CMAKE_SHARED_LIBRARY_PREFIX}FAST${CMAKE_SHARED_LIBRARY_SUFFIX})
+    set(FAST_INCLUDE_DIR ${ECHOBOT_EXTERNAL_BUILD_DIR}/FAST/fast/include)
+    set(FAST_LIBRARY_DIR ${ECHOBOT_EXTERNAL_BUILD_DIR}/FAST/lib)
+    link_directories(${FAST_LIBRARY_DIR})
 endif()
 
-list(APPEND LIBRARIES ${FAST_LIBRARY})
+list(APPEND ECHOBOT_INCLUDE_DIRS ${FAST_INCLUDE_DIR})
+list(APPEND ECHOBOT_INCLUDE_DIRS ${OpenCL_INCLUDE_DIR})
+list(APPEND LIBRARIES ${FAST_LIBRARY} pthread)
 list(APPEND ECHOBOT_EXTERNAL_DEPENDENCIES FAST)
