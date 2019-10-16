03dfa93df1dce66bcad38e2e616a6171bed5241a
androst <a.ostvik@gmail.com>
2019-10-16 09:24:50 +0200

    Updated with semaphoric queue system for data dump.

--- source/EchoBot/Utilities/RecordTool.cpp
+++ source/EchoBot/Utilities/RecordTool.cpp
@@ -2,12 +2,18 @@
 
 #include <FAST/Exporters/VTKMeshFileExporter.hpp>
 #include <FAST/Exporters/MetaImageExporter.hpp>
+#include "FAST/Streamers/ImageFileStreamer.hpp"
+#include "FAST/Streamers/MeshFileStreamer.hpp"
+
+#include <QString>
+#include <QDir>
 
 namespace echobot {
 
 void RecordTool::addRecordChannel(std::string name, DataChannel::pointer channel)
 {
     mRecordChannels[name] = channel;
+    mLastTimeStamps[name] = 0;
 
     uint port = getNrOfInputConnections();
     createInputPort<DataObject>(port);
@@ -24,12 +30,54 @@ void RecordTool::startRecording(std::string path) {
     for (auto ch: mRecordChannels)
         createDirectories((mStoragePath + "/" + ch.first));
 
+    m_fillCount = std::make_unique<LightweightSemaphore>(0);
+    m_emptyCount = std::make_unique<LightweightSemaphore>(1000);
+
     mRecording = true;
-    mRecordThread = new std::thread(std::bind(&RecordTool::dataDumpThread, this));
+    mRecordThread = new std::thread(std::bind(&RecordTool::queueForDataDump, this));
+    mDumpThread = new std::thread(std::bind(&RecordTool::dataDumpThread, this));
 }
 
 void RecordTool::stopRecording() {
     mRecording = false;
+}
+
+void RecordTool::startPlayback(std::string path)
+{
+    mRecording = false;
+
+    std::string selectedRecordingPointClouds = path + "/PointClouds/";
+    std::string selectedRecordingImages = path + "/CameraImages/";
+    std::string selectedRecordingUS = path + "/Ultrasound/";
+
+    if(QDir(QString::fromStdString(selectedRecordingPointClouds)).exists())
+    {
+        MeshFileStreamer::pointer meshStreamer = MeshFileStreamer::New();
+        meshStreamer->setFilenameFormat(selectedRecordingPointClouds + "#.vtk");
+        meshStreamer->enableLooping();
+        meshStreamer->update();
+        mPlaybackStreamers["PointCloud"] = meshStreamer;
+    }
+
+    if(QDir(QString::fromStdString(selectedRecordingImages)).exists())
+    {
+        ImageFileStreamer::pointer imageStreamer = ImageFileStreamer::New();
+        imageStreamer->setFilenameFormat(selectedRecordingImages + "Image-2D_#.mhd");
+        imageStreamer->enableLooping();
+        imageStreamer->setSleepTime(100);
+        imageStreamer->update();
+        mPlaybackStreamers["CameraImage"] = imageStreamer;
+    }
+
+    if(QDir(QString::fromStdString(selectedRecordingUS)).exists())
+    {
+        ImageFileStreamer::pointer usImageStreamer = ImageFileStreamer::New();
+        usImageStreamer->setFilenameFormat(selectedRecordingUS + "Image-2D_#.mhd");
+        usImageStreamer->enableLooping();
+        usImageStreamer->setSleepTime(100);
+        usImageStreamer->update();
+        mPlaybackStreamers["Ultrasound"] = usImageStreamer;
+    }
 }
 
 
@@ -41,49 +89,172 @@ RecordTool::RecordTool()
 {
 }
 
-void RecordTool::dataDumpThread()
+void RecordTool::queueForDataDump()
 {
-    while(true)
-    {
-        if(mRecording)
-        {
+    while(true) {
+        if (mRecording) {
+            if(mTimeStampUpdated)
+                m_emptyCount->wait();
+
             {
-                std::lock_guard<std::mutex> lock(mRecordBufferMutex);
-                if(!mRecording)
+                std::unique_lock<std::mutex> lock(mRecordBufferMutex);
+                if (!mRecording)
                     break;
-            }
 
-            std::string frameNr = std::to_string(mFrameCounter);
+                std::map<std::string, SharedPointer<DataObject>> latestData;
+                std::map<std::string, uint64_t>  latestTimeStamps;
 
-            for (auto ch: mRecordChannels) {
-                mLatestData[ch.first] = ch.second->getNextFrame();
-            }
-
-            for(auto data: mLatestData)
-            {
-                auto className = data.second->getNameOfClass();
-
-                if(className == "Mesh")
-                {
-                    VTKMeshFileExporter::pointer meshExporter = VTKMeshFileExporter::New();
-                    meshExporter->setInputData(data.second);
-                    meshExporter->setWriteNormals(false);
-                    meshExporter->setWriteColors(true);
-                    meshExporter->setFilename(mStoragePath + "/" + data.first + "/" + frameNr  + ".vtk");
-                    meshExporter->update();
+                for (auto ch: mRecordChannels) {
+                    latestData[ch.first] = ch.second->getNextFrame();
+                    latestTimeStamps[ch.first] = latestData[ch.first]->getCreationTimestamp();
                 }
-                else if (className == "Image") {
-                    MetaImageExporter::pointer imageExporter = MetaImageExporter::New();
-                    imageExporter->setInputData(data.second);
-                    imageExporter->setFilename(mStoragePath + "/" + data.first + "/" + "Image-2D_" + frameNr + ".mhd");
-                    imageExporter->update();
+
+                mTimeStampUpdated = false;
+                for (auto stamp: latestTimeStamps){
+                    if(stamp.second > mLastTimeStamps[stamp.first]){
+                        mTimeStampUpdated  = true;
+                        mLastTimeStamps[stamp.first] = stamp.second;
+                    }
                 }
+
+                if(mTimeStampUpdated)
+                    mDataQueue.push(latestData);
             }
-            ++mFrameCounter;
-            update();
+
+            if(mTimeStampUpdated)
+                m_fillCount->signal();
         }
     }
 }
+
+
+void RecordTool::dataDumpThread() {
+    while (true) {
+        m_fillCount->wait();
+
+        if (mDataQueue.empty() && mRecording)
+            continue;
+        else if (mDataQueue.empty() && !mRecording)
+            break;
+
+        std::cout << mDataQueue.size() << std::endl;
+        std::string frameNr = std::to_string(mFrameCounter);
+
+        for(auto data: mDataQueue.front())
+        {
+            auto className = data.second->getNameOfClass();
+
+            if(className == "Mesh")
+            {
+                VTKMeshFileExporter::pointer meshExporter = VTKMeshFileExporter::New();
+                meshExporter->setInputData(data.second);
+                meshExporter->setWriteNormals(false);
+                meshExporter->setWriteColors(true);
+                meshExporter->setFilename(mStoragePath + "/" + data.first + "/" + frameNr  + ".vtk");
+                meshExporter->update();
+            }
+            else if (className == "Image") {
+                MetaImageExporter::pointer imageExporter = MetaImageExporter::New();
+                imageExporter->setInputData(data.second);
+                imageExporter->setFilename(mStoragePath + "/" + data.first + "/" + "Image-2D_" + frameNr + ".mhd");
+                imageExporter->update();
+            }
+        }
+        ++mFrameCounter;
+        mDataQueue.pop();
+        m_emptyCount->signal();
+    }
+}
+
+//void RecordTool::dataDumpThread()
+//{
+//    while(true)
+//    {
+//        if(mRecording)
+//        {
+//            {
+//                std::lock_guard<std::mutex> lock(mRecordBufferMutex);
+//                if(!mRecording)
+//                    break;
+//            }
+//
+//            std::map<std::string, SharedPointer<DataObject>> latestData;
+//            std::string frameNr = std::to_string(mFrameCounter);
+//
+//            for (auto ch: mRecordChannels) {
+//                latestData[ch.first] = ch.second->getNextFrame();
+//            }
+//
+//            mDataQueue.push(latestData);
+//
+//            for(auto data: latestData)
+//            {
+//                auto className = data.second->getNameOfClass();
+//
+//                if(className == "Mesh")
+//                {
+//                    VTKMeshFileExporter::pointer meshExporter = VTKMeshFileExporter::New();
+//                    meshExporter->setInputData(data.second);
+//                    meshExporter->setWriteNormals(false);
+//                    meshExporter->setWriteColors(true);
+//                    meshExporter->setFilename(mStoragePath + "/" + data.first + "/" + frameNr  + ".vtk");
+//                    meshExporter->update();
+//                }
+//                else if (className == "Image") {
+//                    MetaImageExporter::pointer imageExporter = MetaImageExporter::New();
+//                    imageExporter->setInputData(data.second);
+//                    imageExporter->setFilename(mStoragePath + "/" + data.first + "/" + "Image-2D_" + frameNr + ".mhd");
+//                    imageExporter->update();
+//                }
+//            }
+//            ++mFrameCounter;
+//            update();
+//        }
+//    }
+//}
+
+
+void RecordTool::addDataToQueue(std::map<std::string, SharedPointer<DataObject>> data)
+{
+    m_emptyCount->wait();
+
+    {
+        std::unique_lock<std::mutex> lock(mRecordBufferMutex);
+
+        // If stop is signaled, throw an exception to stop the entire computation thread
+        if(mStop)
+            throw ThreadStopped();
+
+        mDataQueue.push(data);
+    }
+
+    // Decrement semaphore by one, signal any waiting due to empty queue
+    m_fillCount->signal();
+}
+
+RecordTool::DataContainer RecordTool::getNextDataFromQueue()
+{
+
+    RecordTool::DataContainer data;
+    {
+        std::unique_lock<std::mutex> lock(mRecordBufferMutex);
+
+        // If stop is signaled, throw an exception to stop the entire computation thread
+        if(mStop)
+            throw ThreadStopped();
+
+        // Get frame next in queue and remove it from the queue
+        data = mDataQueue.front();
+        mDataQueue.pop();
+    }
+
+    // Increment semaphore by one and signal any waiting for next frame due to empty queue
+    m_emptyCount->signal();
+
+    return data;
+}
+
+
 
 uint RecordTool::getFramesStored() const {
     return mFrameCounter;
--- source/EchoBot/Utilities/RecordTool.h
+++ source/EchoBot/Utilities/RecordTool.h
@@ -4,7 +4,10 @@
 #include "EchoBot/Core/SmartPointers.h"
 #include "EchoBot/Interfaces/SensorInterface.h"
 #include "FAST/ProcessObject.hpp"
+#include "FAST/Streamers/Streamer.hpp"
+#include "FAST/Semaphore.hpp"
 #include <thread>
+#include <queue>
 
 class QElapsedTimer;
 
@@ -21,24 +24,43 @@ class RecordTool : public ProcessObject {
         void startRecording(std::string path);
         void stopRecording();
 
+        void startPlayback(std::string path);
+        void stopPlayback();
+
         uint getFramesStored() const;
         bool isRecording() const;
 
     private:
         RecordTool();
         void execute();
+
+
         void dataDumpThread();
+        void queueForDataDump();
 
         std::map<std::string, DataChannel::pointer> mRecordChannels;
-        std::map<std::string, SharedPointer<DataObject>> mLatestData;
+        typedef std::map<std::string, SharedPointer<DataObject>> DataContainer;
+
+        void addDataToQueue(DataContainer data);
+        DataContainer getNextDataFromQueue();
+        std::queue<DataContainer> mDataQueue;
+        std::map<std::string, Streamer::pointer> mPlaybackStreamers;
+        std::map<std::string, uint64_t>  mLastTimeStamps;
+        bool mTimeStampUpdated = true;
 
         bool mRecording = false;
+        bool mStop = false;
         std::string mStoragePath;
         uint mFrameCounter;
         QElapsedTimer* mRecordTimer;
 
         std::thread* mRecordThread;
+        std::thread* mDumpThread;
         std::mutex mRecordBufferMutex;
+
+        uint mMaximumNumberOfFrames;
+        std::unique_ptr<LightweightSemaphore> m_fillCount;
+        std::unique_ptr<LightweightSemaphore> m_emptyCount;
 };
 
 }
