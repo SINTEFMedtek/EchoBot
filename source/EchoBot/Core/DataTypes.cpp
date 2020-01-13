0e81d4c0198d2983be56484953f91d8c065454ca
androst <a.ostvik@gmail.com>
2020-01-13 21:07:25 +0100

    Added some namespace declarations.

---
+++ source/EchoBot/Core/DataTypes.cpp
@@ -0,0 +1,5 @@
+//
+// Created by androst on 12.01.20.
+//
+
+#include "DataTypes.h"
---
+++ source/EchoBot/Core/DataTypes.h
@@ -0,0 +1,73 @@
+//
+// Created by androst on 12.01.20.
+//
+
+#ifndef ECHOBOT_DATATYPES_H
+#define ECHOBOT_DATATYPES_H
+
+#include <FAST/ProcessObject.hpp>
+
+#include <FAST/Data/Image.hpp>
+#include <FAST/Data/Mesh.hpp>
+#include <FAST/Data/Color.hpp>
+
+#include <FAST/Streamers/Streamer.hpp>
+#include <FAST/Streamers/RealSenseStreamer.hpp>
+#include <FAST/Streamers/ImageFileStreamer.hpp>
+#include <FAST/Streamers/MeshFileStreamer.hpp>
+
+#include "FAST/Importers/ImageFileImporter.hpp"
+#include "FAST/Importers/VTKMeshFileImporter.hpp"
+
+#include "FAST/Algorithms/SurfaceExtraction/SurfaceExtraction.hpp"
+#include "FAST/Algorithms/ImageResizer/ImageResizer.hpp"
+#include <FAST/Algorithms/NeuralNetwork/SegmentationNetwork.hpp>
+
+#include <FAST/Exporters/FileExporter.hpp>
+#include <FAST/Exporters/MetaImageExporter.hpp>
+
+#include <FAST/Visualization/Window.hpp>
+#include <FAST/Visualization/View.hpp>
+#include <FAST/Visualization/Renderer.hpp>
+#include <FAST/Visualization/VertexRenderer/VertexRenderer.hpp>
+#include <FAST/Visualization/ImageRenderer/ImageRenderer.hpp>
+#include <FAST/Visualization/TriangleRenderer/TriangleRenderer.hpp>
+#include <FAST/Visualization/SegmentationRenderer/SegmentationRenderer.hpp>
+
+namespace echobot{
+    using Eigen::Affine3f;
+    using Eigen::Matrix3f;
+    using Eigen::Vector3f;
+    using Eigen::VectorXf;
+    using Eigen::MatrixXf;
+
+    using fast::ProcessObject;
+    using fast::DataChannel;
+    using fast::DataObject;
+    using fast::Color;
+    using fast::Image;
+    using fast::AffineTransformation;
+    using fast::Renderer;
+    using fast::Streamer;
+    using fast::Mesh;
+    using fast::VertexRenderer;
+    using fast::ImageRenderer;
+    using fast::RealSenseStreamer;
+    using fast::ImageFileStreamer;
+    using fast::MeshFileStreamer;
+    using fast::MeshVertex;
+    using fast::MeshTriangle;
+    using fast::TriangleRenderer;
+    using fast::Window;
+    using fast::View;
+    using fast::FileExporter;
+    using fast::MetaImageExporter;
+    using fast::ImageFileImporter;
+    using fast::ImageResizer;
+    using fast::SurfaceExtraction;
+    using fast::VTKMeshFileImporter;
+    using fast::SegmentationNetwork;
+    using fast::SegmentationRenderer;
+}
+
+#endif //ECHOBOT_DATATYPES_H
