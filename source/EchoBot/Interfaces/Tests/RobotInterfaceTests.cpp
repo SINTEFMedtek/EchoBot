53917dcf51e619d85051cb6ca91171d159be28be
androst <a.ostvik@gmail.com>
2020-01-20 11:36:06 +0100

    Added test for connecting and listening to new robot states. Removed redundant test.

--- source/EchoBot/Interfaces/Tests/RobotInterfaceTests.cpp
+++ source/EchoBot/Interfaces/Tests/RobotInterfaceTests.cpp
@@ -9,7 +9,18 @@ namespace echobot
 {
 
 TEST_CASE("Create RobotInterface object", "[EchoBot][Interfaces]") {
-    RobotInterface::pointer rInterface = echobot::RobotInterface::New();
+    auto robotInterface = echobot::RobotInterface::New();
+}
+
+TEST_CASE("Connect and listen to new states", "[EchoBot][Interfaces]"){
+    auto robotInterface = echobot::RobotInterface::New();
+    robotInterface->setConfiguration(Manipulator::UR10, "10.218.180.32", 30003);
+    robotInterface->connect();
+
+    for(int i = 0; i<50; i++)
+    {
+        std::cout << robotInterface->getCurrentState()->getJointConfig().transpose() << std::endl;
+    }
 }
 
 }
