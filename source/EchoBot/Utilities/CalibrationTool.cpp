5f9aaff3b2a1972e537af91fb1efd1b0cebb5c37
androst <a.ostvik@gmail.com>
2020-01-21 08:52:20 +0100

    Updated with cal file loader/save and setters.

--- source/EchoBot/Utilities/CalibrationTool.cpp
+++ source/EchoBot/Utilities/CalibrationTool.cpp
@@ -1,39 +1,64 @@
 #include "CalibrationTool.h"
 
 #include <Eigen/Dense>
-
+#include <fstream>
+#include <iostream>
 
 namespace echobot {
 
 
 void CalibrationTool::calibrate() {
-    Eigen::Affine3d rMb = Eigen::Affine3d::Identity();
 
-    Eigen::Matrix3d m;
-    m = Eigen::AngleAxisd(-0.5*M_PI, Eigen::Vector3d::UnitZ())*
-        Eigen::AngleAxisd(1.0*M_PI, Eigen::Vector3d::UnitX())*
-        Eigen::AngleAxisd(0.0*M_PI, Eigen::Vector3d::UnitY());
-
-    // 0.51*M_PI, 0.96*M_PI, 0.0*M_PI
-
-    rMb.translate(Eigen::Vector3d(600, -550, 1520)); // -500, 370, 1000 (y,x,z)
-    rMb.linear() = rMb.linear()*m;
-
-    m_rMb = rMb;
-
-    Eigen::Affine3d eeMt = Eigen::Affine3d::Identity();
-
-    Eigen::Matrix3d rotProbe;
-    rotProbe = Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitZ())*
-               Eigen::AngleAxisd(-M_PI_2, Eigen::Vector3d::UnitX())*
-               Eigen::AngleAxisd(0, Eigen::Vector3d::UnitY());
-
-    eeMt.translate(Eigen::Vector3d(0,-65,130));
-    eeMt.linear() = eeMt.linear()*rotProbe;
-
-    m_eeMt = eeMt;
 }
 
+Eigen::Affine3d CalibrationTool::loadCalFile(std::string path) {
+    auto calMat = Eigen::Affine3d::Identity();
+    std::ifstream inFile;
+    inFile.open(path);
+
+    if(inFile.is_open())
+    {
+        for(int row = 0; row < calMat.rows(); row++){
+            for(int col = 0; col < calMat.cols(); col++)
+            {
+                double item = 0;
+                inFile >> item;
+                calMat(row, col) = item;
+            }
+        }
+        inFile.close();
+    }
+    return calMat;
+}
+
+void CalibrationTool::saveCalFile(std::string path, Eigen::Affine3d calMat) {
+    std::ofstream outFile;
+    outFile.open(path);
+
+    if(outFile.is_open()){
+        outFile << calMat.matrix();
+        outFile.close();
+    }
+}
+
+CalibrationTool::CalibrationTool() {
+    mCalibrationFilePath = Config::getConfigPath() + "calibration/";
+    m_rMb = loadCalFile(mCalibrationFilePath + "camMbase.cal");
+    m_eeMt = loadCalFile(mCalibrationFilePath + "eeMtool.cal");
+    m_tMus = loadCalFile(mCalibrationFilePath + "toolMus.cal");
+}
+
+void CalibrationTool::set_rMb(Eigen::Affine3d mat) {
+    m_rMb = mat;
+}
+
+void CalibrationTool::set_eeMt(Eigen::Affine3d mat) {
+    m_eeMt = mat;
+}
+
+void CalibrationTool::set_tMus(Eigen::Affine3d mat) {
+    m_tMus = mat;
+}
 
 }
 
--- source/EchoBot/Utilities/CalibrationTool.h
+++ source/EchoBot/Utilities/CalibrationTool.h
@@ -3,6 +3,7 @@
 
 #include <eigen3/Eigen/Dense>
 #include "EchoBot/Core/SmartPointers.h"
+#include "EchoBot/Core/Config.h"
 
 
 namespace echobot {
@@ -12,15 +13,25 @@ class CalibrationTool {
 
     public:
         void calibrate();
-
         Eigen::Affine3d get_rMb(){ return m_rMb;};
         Eigen::Affine3d get_eeMt(){ return m_eeMt;};
         Eigen::Affine3d get_tMus(){ return m_tMus;};
 
+        void set_rMb(Eigen::Affine3d mat);
+        void set_eeMt(Eigen::Affine3d mat);
+        void set_tMus(Eigen::Affine3d mat);
+
+        std::string getCalibrationFilePath(){return mCalibrationFilePath;};
+        void setCalibrationFilePath(std::string path){mCalibrationFilePath = path;};
+
+        static Eigen::Affine3d loadCalFile(std::string path);
+        static void saveCalFile(std::string path, Eigen::Affine3d calMat);
+
     private:
         Eigen::Affine3d m_rMb, m_eeMt, m_tMus;
+        std::string mCalibrationFilePath;
 
-        CalibrationTool(){};
+        CalibrationTool();
         std::weak_ptr<CalibrationTool> mPtr;
 };
 
